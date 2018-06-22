3fcdeeae74e8b8867c803737571f5adf81b1bbde
s f <steptan+gkr@gmail.com>
2018-06-22 14:10:34 -0600

    I wrote the tests and watched them fail. Then wrote the code to make them work. *then* I read that we did not necessarily need to make them work, just write the tests. Each one failed before I made it succeed (see the commented out regular expressions which represent the stepping stones along the way)

--- library.py
+++ library.py
@@ -2,9 +2,38 @@ import re
 
 _whole_word = lambda x: re.compile(r'(?<=\W)' + x + '(?=\W)')
 _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
-_integer_pat = _whole_word(r'\d+')
+# _integer_pat = _whole_word(r'\d+')
+_integer_pat = _whole_word(r'\d+[\d,]+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+# _date_with_month_abbrev_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec),? \d{4}')
+_date_with_month_abbrev_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec),? \d{4}')
+# _date_iso8601_pat = _whole_word(r'\d{4}-\d{2}-\d{2}')
+# _date_iso8601_pat = re.compile(r'2018-06-22 18:22:19.123')
+# _date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?')
+# _date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?(([-|+])|( [A-Z]{1,3}))?')
+# _date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?(\+0100|( [A-Z]{1,3}))?')
+# _date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?(\+\d+|( [A-Z]{1,3}))?')
+_date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?([\+|-]\d+|( [A-Z]{1,3}))?')
+# _date_iso8601_pat = re.compile(r'(\d{4}-(0\d|1[012])-(0[1-9]|[12][0-9]|3[01]))(( |T)(\d+:)+([\d.]+)?)?')
+
+
+def dates_iso8601(text):
+    ##  When you're matching ISO 8601 dates, you should also handle dates
+    ##  with timestamps, like "2018-06-22 18:22:19.123". These dates might
+    ##  end with a time precision of minutes, or of seconds, or of milliseconds.
+    ##  The delimiter between the date and the time portion could be either
+    ##  a space or 'T'. And there might be a timezone specifier on the end--
+    ##  either a 3-letter abbreviation like "MDT" or the single letter "Z"
+    ##  (for "Zulu" or UTC), or an offset like "-0800".
+    '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date month', match)
+
+def dates_month_abbrev(text):
+    '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
+    for match in _date_with_month_abbrev_pat.finditer(text):
+        yield('date', match)
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
--- library_test.py
+++ library_test.py
@@ -15,7 +15,7 @@ class TestCase(unittest.TestCase):
     # Helper function
     def assert_extract(self, text, extractors, *expected):
         actual = [x[1].group(0) for x in library.scan(text, extractors)]
-        self.assertEquals(str(actual), str([x for x in expected]))
+        self.assertEqual(str(actual), str([x for x in expected]))
 
     # First unit test; prove that if we scan NUM_CORPUS looking for mixed_ordinals,
     # we find "5th" and "1st".
@@ -26,10 +26,85 @@ class TestCase(unittest.TestCase):
     def test_integers(self):
         self.assert_extract(NUM_CORPUS, library.integers, '1845', '15', '20', '80')
 
+    # Second unit test; prove we find integers with commas
+    def test_integers_with_commas(self):
+        integers_with_commas = 'how many balloons? 111,222,333 ballons! How many ice creams? 1,234.'
+        self.assert_extract(integers_with_commas, library.integers, '111,222,333', '1,234')
+
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+
+    # unit test; prove that invalid months are ignored
+    def test_ignore_invalid_months_and_days(self):
+        invalid_month_str = 'Being from mars, I was born on 2015-89-54.'
+        self.assert_extract(invalid_month_str, library.dates_iso8601)
+
+    # unit test; find dates format DD mmm YYYY
+    def test_simple_date_format(self):
+        valid_date_str = 'Other date format 22 Jun 2018.'
+        self.assert_extract(valid_date_str, library.dates_month_abbrev, '22 Jun 2018')
+
+    # unit test; find dates format DD mmm YYYY
+    def test_simple_date_format_with_comma(self):
+        date='22 Jun, 2018'
+        valid_date_str = 'Other date format: ' + date + ' found here.'
+        self.assert_extract(valid_date_str, library.dates_month_abbrev, date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DD
+    def test_iso_01(self):
+        simple_str = 'I was born on 2015-07-25.'
+        ## self.assert_extract(simple_str, r'\d\d\d\d-\d\d-\d\d', '2015-07-25')
+        self.assert_extract(simple_str, library.dates_iso8601, '2015-07-25')
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DD dd:dd:dd.ddd
+    def test_iso_02(self):
+        iso_date='2018-06-22 18:22:19.123'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd.ddd (with T)
+    def test_iso_03(self):
+        iso_date='2018-06-22T18:22:19.123'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd (minutes)
+    def test_iso_04(self):
+        iso_date='2018-06-22T18:22'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd (seconds)
+    def test_iso_05(self):
+        iso_date='2018-06-22T18:22:19'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd (milliseconds)
+    def test_iso_06(self):
+        iso_date='2018-06-22T18:22:19'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd MDT
+    def test_iso_06(self):
+        iso_date='2018-06-22T18:22:19 MDT'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd+0100
+    def test_iso_07(self):
+        iso_date='2018-06-22T18:22:19+0100'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
+
+    # unit test; prove that we find iso dates in the form YYYY-MM-DDTdd:dd:dd-0800
+    def test_iso_08(self):
+        iso_date='2018-06-22T18:22:19-0800'
+        test='Here is the date ' + iso_date + ' right here.'
+        self.assert_extract(test, library.dates_iso8601, iso_date)
 
 if __name__ == '__main__':
     unittest.main()
